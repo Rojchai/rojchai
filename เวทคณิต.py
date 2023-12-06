64209b6b84c35a313cd9632546f4c408ebffeb1b
Rojchai <110082568+Rojchai@users.noreply.github.com>
2023-12-06 19:28:03 +0700

    Create เวทคณิต.py

---
+++ เวทคณิต.py
@@ -0,0 +1,132 @@
+#พระสูตรเวทคณิต
+#16บิต
+import random
+import math
+while True:
+    tr = random.randint(1, 16)
+    if tr == 1 :
+        print("yes")
+        tr1 = random.randint(1, 2)
+        if tr1 == 1:
+            tr11 = random.randint(1, 2)
+            if tr11 == 1:
+                tr111 = random.randint(1, 5)
+                if tr111 == 1:
+                    print("            ⅓")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/3
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr111 == 2:
+                    print("            ⅐")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/7
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr111 == 3:
+                    print("            ⅑")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/9
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr111 == 4:
+                    print("            1/11")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/11
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr111 == 5:
+                    print("            1/13")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/13
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+            if tr11 == 2:
+                tr112 = random.randint(1,9)
+                if tr112 == 1:
+                    print("            1/19")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/19
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 2:
+                    print("            1/29")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/29
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 3:
+                    print("            1/39")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/39
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 4:
+                    print("            1/49")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/49
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 5:
+                    print("            1/59")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/59
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 6:
+                    print("            1/69")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/69
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 7:
+                    print("            1/79")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/79
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 8:
+                    print("            1/89")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/89
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+                elif tr112 == 9:
+                    print("            1/99")
+                    aw = float(input("ตอบเลยครับ"))
+                    ohn = 1/99
+                    if aw == ohn:
+                        print("yes")
+                    else :
+                        print("no",ohn)
+            
+        if tr1 == 2:
+            continue
+    else:
+        continue
